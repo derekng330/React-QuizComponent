1e812544c2a52a79f9bf1158106888a772356a0a
Derek Ng <derekng330@gmail.com>
2020-07-19 23:54:45 -0700

    module 2

---
+++ src/QuizQuestion.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class QuizQuestion extends Component {
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>// instruction goes here</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        //quiz question
+                    </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQuestion
