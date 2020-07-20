513484801f880ac7bbcb69813cc6ebbcffeb91e9
Derek Ng <derekng330@gmail.com>
2020-07-20 02:49:25 -0700

    Adding Events and Handlers

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,13 +6,13 @@ let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
     render() {
-        const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length;
+        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         return (
             <div>
                     {isQuizEnd && <QuizEnd />}
                     
                     {!isQuizEnd && <QuizQuestion
-                        quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}
                     />}
             </div>
         )
@@ -22,6 +22,10 @@ class Quiz extends Component {
     constructor(props) {
         super(props)
         this.state = {quiz_position: 1}
+    }
+
+    showNextQuestion() {
+
     }
 }
 
