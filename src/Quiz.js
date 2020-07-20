4d283f8277000d0ab4a2b1cb16669c4ac903e9ae
Derek Ng <derekng330@gmail.com>
2020-07-20 00:15:20 -0700

    fixes 3

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,7 +8,7 @@ class Quiz extends Component {
         return (
             <div>
                 <QuizQuestion 
-                    quiz_position={quizData.quiz_questions[this.state.quiz_position-1]}
+                    quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
                 />
             </div>
         )
