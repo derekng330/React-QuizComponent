b930d5037076482294fe94a6e066200ed0373618
Derek Ng <derekng330@gmail.com>
2020-07-19 19:44:46 -0700

    Added Quic Component

---
+++ src/Quiz.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+    render() {
+        <div><div className="QuizQuestion" /></div>
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {quiz_position: 1}
+    }
+}
+
+export default Quiz
